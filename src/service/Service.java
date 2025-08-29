5651b5b939850e4a9e57858d406803681029caf8
Juwoojae <juwqq1234@naver.com>
2025-08-29 15:27:24 +0900

    3차 수정

--- src/service/Service.java
+++ src/service/Service.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public interface Service {
     void selectList();//동물의 목록 조회 메서드
-    Animal selectAnimal() throws IOException; //동물의 목록 조회 메서드
+    Animal selectAnimal() throws IOException; //동물 목록 조회후 동물 선택후 리턴하는 메서드
     int command() throws IOException; //command 를 입력 받는 메서드
     void menu(int cmd) throws IOException; //사용자 메뉴 콘솔 화면 cmd 를 받아서 처리하는 메서드
 }
